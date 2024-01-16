949670873e3322925af4ab8a07b8e62297a6a204
Olya <122615170+olyalupatsa@users.noreply.github.com>
2024-01-16 21:54:10 +0200

    Update 1-gallery.js

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,5 +1,6 @@
-import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
 const images = [
   {
     preview:
